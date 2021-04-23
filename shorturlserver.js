132293d833533bf2959489a3ecc46ce51d26284d
shahidamidhat <78902327+shahidamidhat@users.noreply.github.com>
2021-04-24 00:37:42 +0530

    Update shorturlserver.js

--- shorturlserver.js
+++ shorturlserver.js
@@ -1,124 +1,89 @@
-const express = require("express");
+const cors = require('cors');
+const express = require('express')
+const { nanoid } = require('nanoid')
+const mongodb = require('mongodb');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const app = express();
-const cors = require("cors");
-const {nanoid} =  require("nanoid");
-const mongodb = require("mongodb");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-require ('dotenv').config();
-const URL = process.env.DB;
-const DB = "UrlShortening";
 const port = 3001;
+const URL = process.env.DB;
+const DB = 'urls';
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-
-
-
-app.post("/register",async function(req,res){
+app.post('/register', async(req, res) => {
     try {
-        let connection = await mongodb.connect(URL);
+        let connection = await mongodb.connect(URL, {useUnifiedTopology : true});
         let db = connection.db(DB);
-
-        
-        let isEmailUnique = await db.collection("users").findOne({email : req.body.email})
-        if(isEmailUnique) {
-            res.status(401).json({
-                message: "User Already Exists"
-            })
-        }
-        else{
-            //generating salt
-            let salt = await bcrypt.genSalt(10)
-
-            //hash the password
-            let hash = await bcrypt.hash(req.body.password,salt)
-            //storing hash instead of raw password
-            req.body.password = hash
-
-            let users = await db.collection("users").insertOne(req.body)
-            await connection.close()
-            res.json(
-                {
-                    message: "User Registered"
-                }
-            )
-        }
-    }
-    catch (error){
-        console.log(error)
+        let salt = await bcrypt.genSalt(10)
+        let hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+        await db.collection('users').insertOne(req.body)
+        res.json({
+            message : 'User Registered'
+        })
+    } catch (error) {
+        console.log(error);
     }
 })
 
-app.post("/login",async function(req,res){
+app.post('/login', async (req, res) => {
     try {
-        let connection = await mongodb.connect(URL);
+        let connection = await mongodb.connect(URL,{useUnifiedTopology : true});
         let db = connection.db(DB);
-
-        //find the username or useremail
-        let user = await db.collection("users").findOne({email : req.body.email})
-         
-        //hashing the password and matching with that of user
-
+        let user = await db.collection('users').findOne({email : req.body.email})
         if(user){
-            let isPassCorrect = await bcrypt.compare(req.body.password, user.password)
-             
-            if (isPassCorrect){
-
-                //generating jwt token
-                let token = jwt.sign({id : user._id},process.env.SECRET)
-                //pass token
+            let isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+            if(isPasswordCorrect){
+                let token = jwt.sign({_id : user._id}, process.env.SECRET);
                 res.json({
-                    message:"Allow User",
+                    message : "Allow",
                     token : token,
                     id : user._id
                 })
             }
             else{
-                res.status(404).json({
-                    message: "Email or Password is Incorrect"
+                res.json({
+                    message : "Email or Password is incorrect"
                 })
             }
         }
         else{
-            res.status(404).json({
-                message: "Email or Password is Incorrect"
+            res.json({
+                message : "Email or Password is incorrect"
             })
         }
-
-    }
-    catch(error){
-        console.log(error)
+    } catch (error) {
+        console.log(error);
     }
 })
 
-function authenticate(req,res,next){
-    //to check presence of token
-    if(req.headers.authorization){        
-        //check validity of token
+function authenticate(req, res, next){
+    if(req.headers.authorization){
         try {
-            let jwtValid = jwt.verify(req.headers.authorization,process.env.SECRET)
+            let jwtValid = jwt.verify(req.headers.authorization, process.env.SECRET)
             if(jwtValid){
-                req.userid = jwtValid._id;
-                next()
+                req.userID = jwtValid._id;
+                next();
             }
-        }
-        catch(error){
+        } catch (error) {
             res.status(401).json({
-                message:"Invalid token"
+                message : "Invalid Token"
             })
         }
-    }else {
+    }
+    else{
         res.status(401).json({
-            message:"No token found"
+            message : "No Token Present"
         })
     }
 }
 
-app.get('/shortUrls/:id', authenticate, async (req, res) => {
+app.get('/urls/:id', authenticate, async (req, res) => {
     try {
-        let connection = await mongodb.connect(URL);
+        let connection = await mongodb.connect(URL, {useUnifiedTopology : true});
         let db = connection.db(DB);
         let userData = await db.collection('users').findOne({_id : mongodb.ObjectID(req.params.id)});
         res.json(userData);
@@ -128,14 +93,14 @@ app.get('/shortUrls/:id', authenticate, async (req, res) => {
     }
 })
 
-app.post('/shortUrls/:id', authenticate, async (req, res) => {
+app.post('/urls/:id', authenticate, async (req, res) => {
     try {
-        let connection = await mongodb.connect(URL);
+        let connection = await mongodb.connect(URL, {useUnifiedTopology : true});
         let db = connection.db(DB);
         await db.collection('users').updateOne({_id : mongodb.ObjectID(req.params.id)},{$push : {links : {$each : [{longURL : req.body.longURL , shortURL : nanoid(6)}]}}});
         await connection.close();
         res.json({
-            message : "Short URL Created"
+            message : "URL Created"
         })
     } catch (error) {
         console.log(error);
@@ -144,7 +109,7 @@ app.post('/shortUrls/:id', authenticate, async (req, res) => {
 
 app.get('/:id', async (req, res) => {
     try {
-        let connection = await mongodb.connect(URL);
+        let connection = await mongodb.connect(URL, {useUnifiedTopology : true});
         let db = connection.db(DB);
         let index = req.params.id.substr(7);
         let short = req.params.id.substr(0,6);
@@ -155,4 +120,4 @@ app.get('/:id', async (req, res) => {
     }
 })
 
-app.listen(process.env.PORT || port)
+app.listen(process.env.PORT || port);
